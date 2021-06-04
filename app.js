d18fc9851677e321a090bb781fcf5cf1bca45545
milan.forrat <milan.forrat@gmail.com>
2021-06-04 15:55:34 +0200

    feat: working + local storage + wip on refresh bugs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let enterButton = document.getElementById('enter');
 let input = document.getElementById('user-input'); 
 let ul = document.querySelector('ul');
 let item = document.getElementsByTagName('li');
+let resetBtn = document.querySelector('.reset');
 
 // fonction qui retourne le nombre de characters
 function inputLength(){
@@ -9,16 +10,16 @@ function inputLength(){
 }
 
 function createListElement(){
-
     let li = document.createElement('li');
     // ajoute un texte au li
     li.appendChild(document.createTextNode(input.value));
     // ajoute le li à l'ul
     ul.appendChild(li);
+    // on lance l'exécution du storage
+    storage();
     // on vide la valeur de l'input une fois que le li est ajouté (pour avoir la barre de saisie vide)
     input.value = "";
-
-
+     
     function endTask(){
         // le toggle permet de faire l'effet on/off
         li.classList.toggle("done");
@@ -26,18 +27,24 @@ function createListElement(){
 
     // on met une écoute sur le click du li, qui exécutera la fonction endTask
     li.addEventListener("click", endTask);
+    let ulNumberOfChildren = ul.childElementCount;
+    if(ulNumberOfChildren > 1 && ulNumberOfChildren < 3){
+        resetBtn.style.display = "block";
+        resetBtn.addEventListener('click', (e) => {
+            if(confirm("Voulez-vous vraiment effacer toutes vos tâches en cours ?")){
+                window.localStorage.clear();
+                ul.innerHTML ="";
+                resetBtn.style.display = "none";
+            }
+        })
+    }
 
     let deleteBtn = document.createElement("button");
-    deleteBtn.className = "delete-btn";
     deleteBtn.appendChild(document.createTextNode("X"));
     li.appendChild(deleteBtn);
-    deleteBtn.addEventListener("click", deleteListItem);
-
-    //fonction qui efface un li
-    function deleteListItem(){
-        // je lance un display none en css
-        li.classList.add('delete');
-    }
+    deleteBtn.addEventListener('click', () => {
+        li.remove();
+    });
 }
 
 function addListAfterClick(){
@@ -45,6 +52,9 @@ function addListAfterClick(){
     if(inputLength() > 0 ){
         // alors je peux créer un élément de liste
         createListElement();
+    }
+    else{
+        alert("Vous ne pouvez ajouter une tâche vide");
     }
 }
 
@@ -54,6 +64,19 @@ function addListAfterKeyPress(e){
         createListElement();
     }
 }
+
+// 
+function storage(){
+    window.localStorage.todoList = ul.innerHTML;
+}
+
+function getValues(){
+    let storageContent = window.localStorage.todoList;
+    if(storageContent){
+        ul.innerHTML = storageContent;
+    }
+}
+getValues()
 
 enterButton.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeyPress);
